47d9e2a3e4889fcbe36e8dc3326a2283c328ffca
Md Sahil <287mdsahil@gmail.com>
2020-04-10 21:20:37 +0530

    Initial commit for student class

---
+++ php/classes/student.class.php
@@ -0,0 +1,21 @@
+<?php
+
+class student {
+
+	// Personal details
+	private string $name;
+	private string $dateOfBirth;
+	private string $fatherName;
+	private string $motherName;
+	private string $gurdianName;
+
+	// Contact info
+	private string $email;
+	private string $phoneNumber;
+	private string $address;
+
+	// Academic info
+
+	// Course info	
+	
+}
